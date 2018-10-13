dfb9c2e9c940309e51036eabacdfa55f3d8a7268
Al Davidson <apdavidson@gmail.com>
2018-10-13 15:54:52 +0100

    full spec for download_service

--- spec/services/download_service_spec.rb
+++ spec/services/download_service_spec.rb
@@ -95,6 +95,83 @@ describe DownloadService do
     it 'returns the path' do
       expect(described_class.download(args)).to eq(path + '/file.ext')
     end
+  end
 
+
+  describe '.download_in_parallel' do
+    let(:path) { '/the/file/path' }
+    let(:mock_request) { double('request', run: 'run result') }
+
+    before do
+      allow(described_class).to receive(:construct_request).and_return(mock_request)
+      allow(described_class).to receive(:file_path_for_download).and_return(path + '/file.ext')
+    end
+
+    context 'when no target_dir is given' do
+      before do
+        allow(Dir).to receive(:mktmpdir).and_return('/a/new/temp/dir')
+      end
+
+      it 'makes a temp dir' do
+        expect(Dir).to receive(:mktmpdir)
+        described_class.download_in_parallel(urls: [url])
+      end
+    end
+
+    context 'when a target_dir is given' do
+      let(:target_dir) { '/my/tmp/dir' }
+
+      it 'does not make a temp dir' do
+        expect(Dir).to_not receive(:mktmpdir)
+        described_class.download_in_parallel(urls: [url], target_dir: target_dir)
+      end
+    end
+
+    context 'given an array of urls' do
+      let(:url1) { 'https://my.domain/some/path/file.ext' }
+      let(:url2) { 'https://another.domain/some/otherfile.ext' }
+      let(:args) { {urls: [url1, url2], target_dir: path} }
+      let(:mock_request_1) { double('request1') }
+      let(:mock_request_2) { double('request2') }
+
+      before do
+        allow(described_class).to receive(:file_path_for_download).with(url: url1, target_dir: path).and_return('/tmp/file1')
+        allow(described_class).to receive(:file_path_for_download).with(url: url2, target_dir: path).and_return('/tmp/file2')
+        allow(described_class).to receive(:construct_request).with(url: url1, file_path: '/tmp/file1').and_return(mock_request_1)
+        allow(described_class).to receive(:construct_request).with(url: url2, file_path: '/tmp/file2').and_return(mock_request_2)
+      end
+
+      describe 'for each url' do
+        it 'gets the file_path_for_download' do
+          expect(described_class).to receive(:file_path_for_download).with(url: url1, target_dir: path).and_return('/tmp/file1')
+          expect(described_class).to receive(:file_path_for_download).with(url: url2, target_dir: path).and_return('/tmp/file2')
+          described_class.download_in_parallel(args)
+        end
+
+        it 'constructs a request, passing the url and file path for download' do
+          expect(described_class).to receive(:construct_request).with(url: url1, file_path: '/tmp/file1')
+          expect(described_class).to receive(:construct_request).with(url: url2, file_path: '/tmp/file2')
+          described_class.download_in_parallel(args)
+        end
+
+        it 'queues the request' do
+          expect(mock_hydra).to receive(:queue).with(mock_request_1)
+          expect(mock_hydra).to receive(:queue).with(mock_request_2)
+          described_class.download_in_parallel(args)
+        end
+      end
+
+      it 'runs the request batch' do
+        expect(mock_hydra).to receive(:run)
+        described_class.download_in_parallel(args)
+      end
+
+      it 'returns the urls mapped to file paths' do
+        expect( described_class.download_in_parallel(args) ).to eq({
+          url1 => '/tmp/file1',
+          url2 => '/tmp/file2'
+        })
+      end
+    end
   end
 end
