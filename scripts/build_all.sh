b443534c14bebb8643263631c9b24a7c562b6bbe
Al Davidson <apdavidson@gmail.com>
2018-10-30 12:12:31 +0000

    add Makefile & re-work build_all script for ECR repos

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -3,12 +3,11 @@
 set -e
 
 REPO_SCOPE=${REPO_SCOPE:-aldavidson}
+TAG=${TAG:- latest}
 
 for TYPE in base api worker
 do
   REPO_NAME=${REPO_SCOPE}/fb-submitter-${TYPE}
   echo "Building ${REPO_NAME}"
-  docker build -f docker/${TYPE}/Dockerfile -t ${REPO_NAME} .
-  echo "Pushing ${REPO_NAME} to dockerhub"
-  docker push ${REPO_NAME}
+  docker build -f docker/${TYPE}/Dockerfile -t ${REPO_NAME}:${TAG} .
 done
