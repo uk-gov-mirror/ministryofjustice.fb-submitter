906654fb471ddc5928e281569c4e124cc55f99a7
Phil Lee <phil.lee@digital.justice.gov.uk>
2019-03-22 12:20:25 +0000

    refactor so tests actually test behaviour

--- spec/services/adapters/service_url_resolver_spec.rb
+++ spec/services/adapters/service_url_resolver_spec.rb
@@ -9,21 +9,9 @@ describe Adapters::ServiceUrlResolver do
   describe '#resolve_uri_to_service' do
     context 'given a URI' do
       let(:uri){ URI.parse('/a/relative/path') }
-      before do
-        allow(subject).to receive(:internal_tld).and_return('my-internal-tld')
-      end
 
-      it 'sets the scheme to the scheme for the right environment from Rails config' do
-        expect(subject.send(:resolve_uri_to_service, uri).scheme).to eq('http')
-      end
-
-      it 'gets the internal TLD for the given service slug and environment slug' do
-        expect(subject).to receive(:internal_tld).with(service_slug, environment_slug).and_return('my-internal-tld')
-        subject.send(:resolve_uri_to_service, uri)
-      end
-
-      it 'sets the port to the internal_service_port from Rails config' do
-        expect(subject.send(:resolve_uri_to_service, uri).port).to eq(3000)
+      it 'resolves correctly' do
+        expect(subject.resolve_uri_to_service(uri).to_s).to eql('http://my-service.formbuilder-services-dev:3000/a/relative/path')
       end
     end
   end
@@ -33,23 +21,15 @@ describe Adapters::ServiceUrlResolver do
       let(:url) { 'https://www.example.com/' }
 
       it 'returns the given URL unmodified' do
-        expect(subject.send(:ensure_absolute_url, url)).to eq(url)
+        expect(subject.ensure_absolute_url(url)).to eq(url)
       end
     end
 
     context 'given a relative URL' do
       let(:url) { '/a/relative/url' }
-      before do
-        allow(subject).to receive(:resolve_uri_to_service).and_return('an absolute URL')
-      end
-
-      it 'calls resolve_uri_to_service' do
-        expect(subject).to receive(:resolve_uri_to_service).with(URI.parse(url))
-        subject.send(:ensure_absolute_url, url)
-      end
 
       it 'returns the resolved URL' do
-        expect(subject.send(:ensure_absolute_url, url)).to eq('an absolute URL')
+        expect(subject.ensure_absolute_url(url)).to eq('http://my-service.formbuilder-services-dev:3000/a/relative/url')
       end
     end
   end
