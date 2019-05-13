916d87ef2d1eafe7e1fe0e9eed660d8033ccc00b
Phil Lee <phil.lee@digital.justice.gov.uk>
2019-05-13 16:49:20 +0100

    change tests to mimic json payload for api calls

--- spec/controllers/save_return/email_confirmations_controller_spec.rb
+++ spec/controllers/save_return/email_confirmations_controller_spec.rb
@@ -2,11 +2,18 @@ require 'rails_helper'
 
 describe SaveReturn::EmailConfirmationsController, :active do
   before :each do
+    request.env['CONTENT_TYPE'] = 'application/json'
     allow_any_instance_of(ApplicationController).to receive(:verify_token!)
   end
 
   let(:email) { 'recipient@example.com' }
   let(:confirmation_link) { 'https://example.com/foo' }
+  let(:json_hash) do
+    {
+      email: email,
+      confirmation_link: confirmation_link
+    }
+  end
 
   describe 'POST #create' do
     it 'enqueues job' do
@@ -17,7 +24,7 @@ describe SaveReturn::EmailConfirmationsController, :active do
 
     it 'sets job params correctly' do
       expect do
-        post :create, params: { email: email, confirmation_link: confirmation_link }
+        post :create, body: json_hash.to_json
       end.to have_enqueued_job(SaveReturnEmailConfirmationJob).with(email: email, confirmation_link: confirmation_link)
     end
 
--- spec/controllers/save_return/email_magic_links_controller_spec.rb
+++ spec/controllers/save_return/email_magic_links_controller_spec.rb
@@ -2,11 +2,18 @@ require 'rails_helper'
 
 describe SaveReturn::EmailMagicLinksController, :active do
   before :each do
+    request.env['CONTENT_TYPE'] = 'application/json'
     allow_any_instance_of(ApplicationController).to receive(:verify_token!)
   end
 
   let(:email) { 'recipient@example.com' }
   let(:magic_link) { 'https://example.com/foo' }
+  let(:json_hash) do
+    {
+      email: email,
+      magic_link: magic_link
+    }
+  end
 
   describe 'POST #create' do
     it 'enqueues job' do
@@ -17,7 +24,7 @@ describe SaveReturn::EmailMagicLinksController, :active do
 
     it 'sets job params correctly' do
       expect do
-        post :create, params: { email: email, magic_link: magic_link }
+        post :create, body: json_hash.to_json
       end.to have_enqueued_job(SaveReturnEmailMagicLinkJob).with(email: email, magic_link: magic_link)
     end
 
