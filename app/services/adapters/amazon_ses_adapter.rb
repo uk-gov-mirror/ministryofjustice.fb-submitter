da8b9092f6f7cde7969a95accc1981cae31b1565
Tom Taylor <tommotaylor@gmail.com>
2019-07-09 16:34:30 +0100

    Remove logging of senstive data from AmazonSESAdapter (#49)
    This logging exposed PII and was only for debug purposes so was removed.

--- app/services/adapters/amazon_ses_adapter.rb
+++ app/services/adapters/amazon_ses_adapter.rb
@@ -3,10 +3,6 @@ module Adapters
     # creds automatically retrieved from
     # ENV['AWS_ACCESS_KEY_ID'] and ENV['AWS_SECRET_ACCESS_KEY']
     def self.send_mail( opts = {} )
-      Rails.logger.debug "send_mail to #{opts[:to]}"
-      Rails.logger.debug "raw_message: #{opts[:raw_message]}"
-      Rails.logger.debug "send_mail from #{opts[:from]}"
-
       client.send_raw_email({
         destinations: [ opts[:to] ],
         raw_message: {
