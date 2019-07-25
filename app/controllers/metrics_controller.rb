a146d6c15132a1e58af1a3e91c8689a881bcc1da
Emile <emile.swarts123@gmail.com>
2019-07-25 09:39:38 +0100

    Restore metrics about background jobs
    This will make the job queue statistics visible in Grafana.
    There are less metrics than Resque provided out of the box but having visibility
    over pending and failed jobs will be enough to recognise if something is going
    wrong.
    
    Could be extended in the future if need be.

--- app/controllers/metrics_controller.rb
+++ app/controllers/metrics_controller.rb
@@ -1,37 +1,24 @@
 class MetricsController < ActionController::Base
-  PrometheusResqueGauges = [
-    { name: :resque_jobs_pending,
-      lookup: :pending,
-      docstring: 'Number of pending jobs' },
-    { name: :resque_jobs_processed,
-      lookup: :processed,
-      docstring: 'Number of jobs processed' },
-    { name: :resque_job_queues,
-      lookup: :queues,
-      docstring: ' Number of job queues' },
-    { name: :resque_workers,
-      lookup: :workers,
-      docstring: 'Number of workers' },
-    { name: :resque_workers_working,
-      lookup: :working,
-      docstring: 'Number of workers working' },
-    { name: :resque_jobs_failed,
-      lookup: :failed,
-      docstring: 'Number of jobs failed' }
-  ].freeze
-
   def show
-    respond_to do |f|
-      f.text
-      f.html { render template: 'metrics/show.text' }
-    end
+    response.headers['version'] = '0.0.4'
+    @stats = delayed_jobs_stats
   end
 
   private
 
-  def prometheus_resque_gauges
-    PrometheusResqueGauges
-  end
+  def delayed_jobs_stats
+    pending_job_count = Delayed::Job.where('attempts = 0').count
+    failed_job_count = Delayed::Job.where('attempts > 0').count
 
-  helper_method :prometheus_resque_gauges
+    [
+      { name: :resque_jobs_pending,
+        docstring: 'Number of pending jobs',
+        value: pending_job_count
+      },
+      { name: :resque_jobs_failed,
+        docstring: 'Number of jobs failed',
+        value: failed_job_count
+      }
+    ]
+  end
 end
