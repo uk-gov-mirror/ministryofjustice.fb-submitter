5e838c485cbb64411d7f8a2cbc1167334d4dfaf3
Phil Lee <phil.lee@digital.justice.gov.uk>
2019-08-14 15:11:07 +0100

    debug body

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,16 @@ class ApplicationController < ActionController::API
   include Concerns::ErrorHandling
   include Concerns::JWTAuthentication
 
+  before_action :debug_body
   before_action :consider_all_requests_json
   before_action :enforce_json_only
 
   private
+
+  def debug_body
+    Rails.logger.warn('======= request.body')
+    Rails.logger.warn(request.body.read)
+  end
 
   def consider_all_requests_json
     if request.env["CONTENT_TYPE"] == 'application/x-www-form-urlencoded'
