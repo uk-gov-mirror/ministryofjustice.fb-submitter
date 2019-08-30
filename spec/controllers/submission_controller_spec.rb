3ff79070e038e0f752eb14b2182981702a232446
cob16 <public+github@cormacbrady.info>
2019-08-30 11:56:26 +0100

    add controller spec for Submission controller

--- spec/controllers/submission_controller_spec.rb
+++ spec/controllers/submission_controller_spec.rb
@@ -2,4 +2,22 @@ require 'rails_helper'
 
 RSpec.describe SubmissionController, type: :controller do
 
+  before :each do
+    request.env['CONTENT_TYPE'] = 'application/json'
+    allow_any_instance_of(ApplicationController).to receive(:verify_token!)
+  end
+
+  it 'creates a submission' do
+    post :create, body: {somthing: 1}.to_json
+
+    expect(Submission.all.count).to eq(1)
+  end
+
+  it 'starts a delayed job' do
+    post :create, body: {somthing: 1}.to_json
+
+    expect(response).to have_http_status(201)
+    expect(Delayed::Job.all.count).to eq(1)
+  end
+
 end
