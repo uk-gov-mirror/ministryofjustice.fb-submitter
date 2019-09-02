f06b5739b2119e7a17d5028e09fdad807bb8694a
cob16 <public+github@cormacbrady.info>
2019-09-02 12:35:34 +0100

    add WebhookDestination adaptor

---
+++ app/services/adapters/webhook_destination.rb
@@ -0,0 +1,26 @@
+module Adapters
+  class WebhookDestination
+
+    class DestinationRequestError < StandardError
+    end
+
+    def initialize(url:)
+      @url = url
+    end
+
+    def send_webhook(body:)
+      response = Typhoeus::Request.new(
+          url,
+          method: :post,
+          body: body
+          # headers: { Accept: "text/html" }
+      ).run
+      unless response.success?
+        raise DestinationRequestError, "request for  #{url} returned response status of: #{response.code}"
+      end
+    end
+
+    private
+    attr_reader :url
+  end
+end
