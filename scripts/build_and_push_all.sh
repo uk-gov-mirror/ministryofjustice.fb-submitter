9ce4d1e47e577ae162075da5c87e690a421641a4
cob16 <public+github@cormacbrady.info>
2019-11-12 17:26:13 +0000

    Revert "Use a single docker file instead of two separate ones (#234)"
    This reverts commit b4ae24ca

--- scripts/build_and_push_all.sh
+++ scripts/build_and_push_all.sh
@@ -21,11 +21,14 @@ login_to_ecr_with_creds_for() {
   eval $(aws ecr get-login --no-include-email --region eu-west-2)
 }
 
-REPO_NAME=${REPO_SCOPE}/fb-submitter-api
-echo "Building ${REPO_NAME}"
-docker build -f docker/api/Dockerfile -t ${REPO_NAME}:${TAG} -t ${REPO_NAME}:${CIRCLE_SHA1} .
+for TYPE in api worker
+do
+  REPO_NAME=${REPO_SCOPE}/fb-submitter-${TYPE}
+  echo "Building ${REPO_NAME}"
+  docker build -f docker/${TYPE}/Dockerfile -t ${REPO_NAME}:${TAG} -t ${REPO_NAME}:${CIRCLE_SHA1} .
 
-login_to_ecr_with_creds_for api
-echo "Pushing ${REPO_NAME}"
-docker push ${REPO_NAME}:${TAG}
-docker push ${REPO_NAME}:${CIRCLE_SHA1}
+  login_to_ecr_with_creds_for ${TYPE}
+  echo "Pushing ${REPO_NAME}"
+  docker push ${REPO_NAME}:${TAG}
+  docker push ${REPO_NAME}:${CIRCLE_SHA1}
+done
--- scripts/build_platform_images.sh
+++ scripts/build_platform_images.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-node_modules/\@ministryofjustice/fb-deploy-utils/bin/build_platform_images.sh $@ --app fb-submitter --images api
+node_modules/\@ministryofjustice/fb-deploy-utils/bin/build_platform_images.sh $@ --app fb-submitter --images api --images worker
