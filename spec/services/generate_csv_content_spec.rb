27f37d7182037fd6db0c2fc38228a8eaa45eee2e
Phil Lee <phil.lee@digital.justice.gov.uk>
2019-12-03 09:08:22 +0000

    fix rubocop

--- spec/services/generate_csv_content_spec.rb
+++ spec/services/generate_csv_content_spec.rb
@@ -14,13 +14,14 @@ describe GenerateCsvContent do
     create(:submission).decrypted_payload
   end
 
+  # rubocop:disable RSpec/ExampleLength
   context 'when requesting a csv with a submission' do
     it 'returns an Attachment object' do
       result = subject.execute
       expect(result.class).to eq(Attachment)
     end
 
-    it 'assigns the correct info the the Attachment object' do
+    it 'assigns the correct info the Attachment object' do
       result = subject.execute
 
       expect(result.filename).to eq("#{payload_service.submission_id}-answers.csv")
@@ -30,33 +31,34 @@ describe GenerateCsvContent do
       csv = CSV.new(file_contents).read
 
       expect(csv[0]).to eql([
-        "submission_id",
-        "usn",
-        "maat[1]",
-        "fullname",
-        "dob",
-        "solicitor_fullname",
-        "firm",
-        "solicitor_email",
-        "lapan",
-        "offence[1].type",
-        "offence[1].date",
-        "offence[1].reasons",
-        "documentation"
+        'submission_id',
+        'usn',
+        'maat[1]',
+        'fullname',
+        'dob',
+        'solicitor_fullname',
+        'firm',
+        'solicitor_email',
+        'lapan',
+        'offence[1].type',
+        'offence[1].date',
+        'offence[1].reasons',
+        'documentation'
       ])
 
-      expect(csv[1]).to eql(["1234567",
-                             "6123456",
-                             "john doe",
-                             "1 January 1990",
-                             "Mr Solicitor",
-                             "Pearson",
-                             "info@solicitor.co.uk",
-                             "1A234B",
-                             "Grand theft auto",
-                             "1 January 1990",
-                             "A genuine reason",
-                             "an-image.jpg"])
+      expect(csv[1]).to eql(['1234567',
+                             '6123456',
+                             'john doe',
+                             '1 January 1990',
+                             'Mr Solicitor',
+                             'Pearson',
+                             'info@solicitor.co.uk',
+                             '1A234B',
+                             'Grand theft auto',
+                             '1 January 1990',
+                             'A genuine reason',
+                             'an-image.jpg'])
     end
   end
+  # rubocop:enable RSpec/ExampleLength
 end
