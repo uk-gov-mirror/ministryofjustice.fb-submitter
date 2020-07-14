4e9ca95eb62a87ee8d77917fcd1dacae2152082a
Tomas D'Stefano <tomas_stefano@successoft.com>
2020-07-14 12:53:01 +0100

    Filter more parameters in the logs
    Below you can see that we are logging more information that we shouldn't
    
    https://kibana.cloud-platform.service.justice.gov.uk/_plugin/kibana/app/kibana#/discover?_g=(refreshInterval:(pause:!t,value:0),time:(from:now-30d,to:now))&_a=(columns:!(_source),index:d1d23280-fc8e-11e9-a859-bbe630739c91,interval:auto,query:(language:kuery,query:'female%20and%20formbuilder'),sort:!(!('@timestamp',desc)))
    
    This commit adds filtering to the logs

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,7 +1,13 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password,
-                                               :encrypted_user_id_and_token,
-                                               :to,
-                                               :subject]
+#
+Rails.application.config.filter_parameters += [
+  :actions,
+  :submission,
+  :attachments,
+  :password,
+  :encrypted_user_id_and_token,
+  :to,
+  :subject
+]
