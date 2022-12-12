403f08786659ff5065f5dadd6c00b335c4902b14
brenetic <brenetic@users.noreply.github.com>
2022-12-12 14:00:42 +0000

    Use reference number in answers pdf name
    If there is a reference number present in the payload use that instead
    of the submission id to create the answers pdf file name.
    
    Fall back to the submission ID if there is no reference number

--- spec/services/pdf_payload_translator_spec.rb
+++ spec/services/pdf_payload_translator_spec.rb
@@ -6,23 +6,49 @@ RSpec.describe PdfPayloadTranslator do
   describe '#to_h' do
     context 'with a valid decrypted submission' do
       let(:submission_id) { SecureRandom.uuid }
-      let(:decrypted_submission) do
-        JSON.parse(
-          File.read(
-            Rails.root.join('spec/fixtures/payloads/valid_submission.json')
-          )
-        ).merge(submission_id: submission_id)
+      let(:valid_submission) do
+        JSON.parse(File.read(Rails.root.join('spec/fixtures/payloads/valid_submission.json')))
       end
-      let(:expected_payload) do
-        {
-          submission: JSON.parse(File.read(
-                                   Rails.root.join('spec/fixtures/payloads/pdf_generator.json')
-                                 )).merge('submission_id' => submission_id)
-        }.deep_symbolize_keys
+      let(:pdf_fixture) do
+        JSON.parse(File.read(Rails.root.join('spec/fixtures/payloads/pdf_generator.json')))
       end
 
-      it 'creates the correct pdf payload' do
-        expect(translator.to_h).to eq(expected_payload)
+      context 'when there is no reference number present' do
+        let(:decrypted_submission) do
+          valid_submission.merge(submission_id: submission_id)
+        end
+        let(:expected_payload) do
+          {
+            submission: pdf_fixture.merge('submission_id' => submission_id)
+          }.deep_symbolize_keys
+        end
+
+        it 'creates the correct pdf payload' do
+          expect(translator.to_h).to eq(expected_payload)
+        end
+      end
+
+      context 'when there is a reference number present' do
+        let(:reference_number) { 'some-reference-number' }
+        let(:decrypted_submission) do
+          sub = valid_submission.merge(submission_id: submission_id)
+          sub['meta']['reference_number'] = reference_number
+          sub
+        end
+        let(:expected_payload) do
+          {
+            submission: pdf_fixture.merge(
+              {
+                'submission_id' => submission_id,
+                'reference_number' => reference_number
+              }
+            )
+          }.deep_symbolize_keys
+        end
+
+        it 'creates the correct pdf payload' do
+          expect(translator.to_h).to eq(expected_payload)
+        end
       end
     end
   end
