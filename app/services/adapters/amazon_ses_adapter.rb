7e47de6a27f68729b3a8495803cb54fa3c63db8c
brenetic <brenetic@users.noreply.github.com>
2022-12-19 11:21:52 +0000

    Use MoJ Forms default no reply email as from address
    This is because we have been getting reports of undelivered messages
    when sent to certain email clients when using certain From Addresses
    that have been set by the user.
    
    Until a more long term fix is found use the MoJ Forms default no reply
    address and set the Editor user's From Address as the Reply to address.
    
    If the from address is the same as the MoJ Forms default no reply
    address then do not set a reply to address in the payload sent to AWS.

--- app/services/adapters/amazon_ses_adapter.rb
+++ app/services/adapters/amazon_ses_adapter.rb
@@ -1,13 +1,16 @@
 module Adapters
   class AmazonSESAdapter
+    DEFAULT_FROM_ADDRESS = 'no-reply-moj-forms@digital.justice.gov.uk'.freeze
+
     # creds automatically retrieved from
     # ENV['AWS_ACCESS_KEY_ID'] and ENV['AWS_SECRET_ACCESS_KEY']
     def self.send_mail(opts = {})
       client.send_email(
-        from_email_address: opts[:from],
+        from_email_address: DEFAULT_FROM_ADDRESS,
         destination: {
           to_addresses: [opts[:to]]
         },
+        reply_to_addresses: ([opts[:from]] - [DEFAULT_FROM_ADDRESS]).compact,
         content: {
           raw: {
             data: opts[:raw_message].to_s
