5c455fc94de038b24002ea104db3ab1d32ba332f
Steven Leighton <steven.leighton@digital.justice.gov.uk>
2024-07-25 11:06:00 +0100

    lint

--- spec/services/v2/replay_batch_submission_spec.rb
+++ spec/services/v2/replay_batch_submission_spec.rb
@@ -1,61 +1,68 @@
 require 'rails_helper'
 
 VALID_DOMAINS =
-[
-  'justice.gov.uk',
-  'digital.justice.gov.uk',
-  'cica.gov.uk',
-  'ccrc.gov.uk',
-  'judicialappointments.gov.uk',
-  'judicialombudsman.gov.uk',
-  'ospt.gov.uk',
-  'gov.sscl.com',
-  'hmcts.net'
-]
+  [
+    'justice.gov.uk',
+    'digital.justice.gov.uk',
+    'cica.gov.uk',
+    'ccrc.gov.uk',
+    'judicialappointments.gov.uk',
+    'judicialombudsman.gov.uk',
+    'ospt.gov.uk',
+    'gov.sscl.com',
+    'hmcts.net'
+  ].freeze
 
 INVALID_DOMAINS =
-[
-  'fake-justice.gov.uk',
-  'digita.justice.gov.uk',
-  'cico.gov.uk'
-]
+  [
+    'fake-justice.gov.uk',
+    'digita.justice.gov.uk',
+    'cico.gov.uk'
+  ].freeze
 
-TWENTY_EIGHT_DAYS_IN_SECONDS = 28*24*60*60
+TWENTY_EIGHT_DAYS_IN_SECONDS = 28 * 24 * 60 * 60
 
 RSpec.describe V2::ReplayBatchSubmission do
-  let(:subject) { described_class.new(date_from:, date_to:, service_slug:, new_destination_email:, resend_json:, resend_mslist:) }
+  let(:replay_batch_submissions) { described_class.new(date_from:, date_to:, service_slug:, new_destination_email:, resend_json:, resend_mslist:) }
 
   let(:date_from) { 3.days.ago.to_s }
-  let(:date_to) { 1.days.ago.to_s }
+  let(:date_to) { 1.day.ago.to_s }
   let(:service_slug) { 'my-service-slug' }
   let(:new_destination_email) { 'valid@justice.gov.uk' }
   let(:resend_json) { false }
   let(:resend_mslist) { false }
 
   describe '#call' do
+    before do
+      allow(replay_batch_submissions).to receive(:process_submissions).and_return(true)
+    end
+
     context 'when there are valid params' do
       it 'calls process submissions' do
-        expect(subject).to receive(:process_submissions).and_return(true)
-        subject.call
+        replay_batch_submissions.call
+
+        expect(replay_batch_submissions).to have_received(:process_submissions)
       end
     end
 
-    context 'dates are invalid' do
-      let(:date_from) { 1.days.ago.to_s }
+    context 'when dates are invalid' do
+      let(:date_from) { 1.day.ago.to_s }
       let(:date_to) { 3.days.ago.to_s }
 
       it 'does not call process submissions' do
-        expect(subject).to_not receive(:process_submissions)
-        subject.call
+        replay_batch_submissions.call
+
+        expect(replay_batch_submissions).not_to have_received(:process_submissions)
       end
     end
 
-    context 'destination invalid' do
+    context 'when destination invalid' do
       let(:new_destination_email) { 'invalid@wrongdomain.gov.uk' }
 
       it 'does not call process submissions' do
-        expect(subject).to_not receive(:process_submissions)
-        subject.call
+        replay_batch_submissions.call
+
+        expect(replay_batch_submissions).not_to have_received(:process_submissions)
       end
     end
   end
@@ -63,20 +70,20 @@ RSpec.describe V2::ReplayBatchSubmission do
   describe '#validate_dates' do
     context 'when dates are valid' do
       it 'returns true' do
-        expect(subject.validate_dates).to be(true)
+        expect(replay_batch_submissions.validate_dates).to be(true)
       end
     end
 
-    context 'dates are valid but not sensible' do
-      let(:date_from) { 1.days.ago.to_s }
+    context 'when dates are valid but not sensible' do
+      let(:date_from) { 1.day.ago.to_s }
       let(:date_to) { 2.days.ago.to_s }
 
       it 'returns false' do
-        expect(subject.validate_dates).to be(false)
+        expect(replay_batch_submissions.validate_dates).to be(false)
       end
     end
 
-    context 'date_from could not be parsed' do
+    context 'when date_from could not be parsed' do
       let(:date_from) { 'NaN' }
 
       it 'throws date error' do
@@ -84,42 +91,41 @@ RSpec.describe V2::ReplayBatchSubmission do
       end
     end
 
-    context 'date_to could not be parsed' do
+    context 'when date_to could not be parsed' do
       let(:date_to) { 'peanut' }
 
       it 'throws date error' do
         expect { described_class.new(date_from:, date_to:, service_slug:, new_destination_email:) }.to raise_error(Date::Error)
       end
     end
-
   end
 
   describe '#validate_destination' do
-    context 'domain is on the allow list' do
+    context 'when domain is on the allow list' do
       VALID_DOMAINS.each do |domain|
         let(:new_destination_email) { "hello@#{domain}" }
 
         it "returns valid for #{domain}" do
-          expect(subject.validate_destination).to be(true)
+          expect(replay_batch_submissions.validate_destination).to be(true)
         end
       end
     end
 
-    context 'domain is not on the allow list' do
+    context 'when domain is not on the allow list' do
       INVALID_DOMAINS.each do |domain|
         let(:new_destination_email) { "hello@#{domain}" }
 
         it "returns valid for #{domain}" do
-          expect(subject.validate_destination).to be(false)
+          expect(replay_batch_submissions.validate_destination).to be(false)
         end
       end
     end
   end
 
   describe '#get_submissions_to_process' do
-    context 'there are submissions' do
+    context 'when there are submissions' do
       before do
-        create(:submission, created_at: Date.today.beginning_of_day, service_slug:)
+        create(:submission, created_at: Time.zone.today.beginning_of_day, service_slug:)
         create(:submission, created_at: 2.days.ago, service_slug:)
         create(:submission, created_at: 2.days.ago, service_slug: 'another-form')
         create(:submission, created_at: 3.days.ago, service_slug:)
@@ -128,15 +134,15 @@ RSpec.describe V2::ReplayBatchSubmission do
 
       it 'retrieves submissions' do
         expect(Submission.count).to be(5)
-        expect(subject.get_submissions_to_process.count).to be(2)
-        expect(subject.get_submissions_to_process.first.service_slug).to eq(service_slug)
-        expect(subject.get_submissions_to_process.last.service_slug).to eq(service_slug)
+        expect(replay_batch_submissions.get_submissions_to_process.count).to be(2)
+        expect(replay_batch_submissions.get_submissions_to_process.first.service_slug).to eq(service_slug)
+        expect(replay_batch_submissions.get_submissions_to_process.last.service_slug).to eq(service_slug)
       end
     end
 
-    context 'there are no submissions' do
+    context 'when there are no submissions' do
       it 'returns empty' do
-        expect(subject.get_submissions_to_process.count).to be(0)
+        expect(replay_batch_submissions.get_submissions_to_process.count).to be(0)
       end
     end
   end
@@ -162,47 +168,47 @@ RSpec.describe V2::ReplayBatchSubmission do
       ActiveJob::Base.queue_adapter.enqueued_jobs = []
     end
 
-    context 'no submissions in date range' do
+    context 'when no submissions in date range' do
       before do
         create(:submission, payload: encrypted_payload, access_token:, created_at: 5.days.ago, service_slug:)
       end
 
       it 'enqueues no jobs' do
-        subject.process_submissions
+        replay_batch_submissions.process_submissions
 
         expect(Submission.all.count).to eq(1)
 
         expect {
-          subject.process_submissions
+          replay_batch_submissions.process_submissions
         }.to change {
           ActiveJob::Base.queue_adapter.enqueued_jobs.count
         }.by 0
 
         expect {
-          subject.process_submissions
+          replay_batch_submissions.process_submissions
         }.to change(Submission, :count).by(0)
       end
     end
 
-    context 'there are submissions' do
+    context 'when there are submissions' do
       let(:reprocessed_submission) { create(:submission, payload: encrypted_payload, access_token:, created_at: 2.days.ago, service_slug:) }
 
-      it 'duplicates the old submission' do
+      it 'duplicates the old submission with new submission and csv actions' do
         old_id = reprocessed_submission.id # this also ensures rspec creates it before we process submisisons
 
-        subject.process_submissions
+        replay_batch_submissions.process_submissions
 
         # select the cloned submission we expect irrespective of ordering
-        new_submission = Submission.select { |s| s.id != old_id }.first
+        new_submission = Submission.all.reject { |s| s.id == old_id }.first
 
         expect(new_submission.decrypted_submission['actions'].find { |a| a['kind'] == 'email' }['to']).to eq(new_destination_email)
         expect(new_submission.decrypted_submission['actions'].find { |a| a['kind'] == 'csv' }['to']).to eq(new_destination_email)
       end
 
       it 'leaves the old submission intact' do
-        old_id = reprocessed_submission.id # this also ensures rspec creates it before we process submisisons
+        reprocessed_submission.id # this also ensures rspec creates it before we process submisisons
 
-        subject.process_submissions
+        replay_batch_submissions.process_submissions
 
         expect(reprocessed_submission.decrypted_submission['actions'].find { |a| a['kind'] == 'email' }['to']).to eq('captain.needa@star-destroyer.com,admiral.piett@star-destroyer.com')
         expect(reprocessed_submission.decrypted_submission['actions'].find { |a| a['kind'] == 'csv' }['to']).to eq('captain.needa@star-destroyer.com,admiral.piett@star-destroyer.com')
@@ -212,25 +218,25 @@ RSpec.describe V2::ReplayBatchSubmission do
       it 'discards other actions by default' do
         old_id = reprocessed_submission.id # this also ensures rspec creates it before we process submisisons
 
-        subject.process_submissions
+        replay_batch_submissions.process_submissions
 
         # select the cloned submission we expect irrespective of ordering
-        new_submission = Submission.select { |s| s.id != old_id }.first
+        new_submission = Submission.all.reject { |s| s.id == old_id }.first
 
         expect(new_submission.decrypted_submission['actions'].count).to eq(2)
       end
 
-      context 'resending non email actions' do
+      context 'when resending non email actions' do
         let(:resend_json) { true }
         let(:resend_mslist) { true }
 
         it 'can resend mslist and json actions if configured' do
           old_id = reprocessed_submission.id # this also ensures rspec creates it before we process submisisons
 
-          subject.process_submissions
+          replay_batch_submissions.process_submissions
 
           # select the cloned submission we expect irrespective of ordering
-          new_submission = Submission.select { |s| s.id != old_id }.first
+          new_submission = Submission.all.reject { |s| s.id == old_id }.first
 
           expect(new_submission.decrypted_submission['actions'].count).to eq(4)
         end
@@ -238,11 +244,11 @@ RSpec.describe V2::ReplayBatchSubmission do
 
       it 'enqueues a job for each submission' do
         old_id = reprocessed_submission.id # this also ensures rspec creates it before we process submisisons
-        subject.process_submissions
-        new_submission = Submission.select { |s| s.id != old_id }.first
+        replay_batch_submissions.process_submissions
+        new_submission = Submission.all.reject { |s| s.id == old_id }.first
 
         expect {
-          subject.process_submissions
+          replay_batch_submissions.process_submissions
         }.to change {
           ActiveJob::Base.queue_adapter.enqueued_jobs.count
         }.by 1
@@ -250,10 +256,9 @@ RSpec.describe V2::ReplayBatchSubmission do
         enqueued_job = ActiveJob::Base.queue_adapter.enqueued_jobs[0]
 
         expect(enqueued_job['arguments'][0]['submission_id']).to eq(new_submission.id)
-        expect(enqueued_job['job_class']).to eq("V2::ProcessSubmissionJob")
+        expect(enqueued_job['job_class']).to eq('V2::ProcessSubmissionJob')
         expect(enqueued_job['arguments'][0]['jwt_skew_override']).to eq(TWENTY_EIGHT_DAYS_IN_SECONDS)
       end
     end
   end
 end
-
